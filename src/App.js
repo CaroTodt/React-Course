d2759e13d1901106e1f8531d71313b627c9442af
Carolina Todt <carolina.todt@rosariosoftware.com>
2021-08-27 16:40:17 -0300

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+
 import './App.css';
+// import Greet from './components/Greet';
+// import Welcome from './components/Welcome';
+// import Hello from './components/Hello';
+// import Message from './components/Message';
+// import Counter from './components/Counter'
+// import FunctionClick from './components/FunctionClick';
+// import ClassClick from './components/ClassClick';
+// import EventBind from './components/EventBind';
+// import ParentComponent from './components/ParentComponent';
+// import UserGreeting from './components/UserGreeting';
+// import NameList from './components/NameList';
+// import Stylesheets from './components/Stylesheets';
+// import Inline from './components/Inline';
+// import './appStyles.css'
+// import styles from './appStyles.module.css'
+// import Form from './components/Form';
+import LifecycleA from './components/LifecycleA';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+<LifecycleA/>
+{/* <Form/> */}
+
+      {/* <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      <Stylesheets primary={true} />
+      <Inline/> */}
+      {/* <NameList /> */}
+      {/* <UserGreeting/> */}
+      {/* <ParentComponent/> */}
+      {/* <EventBind/> */}
+      {/* <FunctionClick />
+      <ClassClick /> */}
+      {/* <Counter/> */}
+      {/* <Greet name="Diana" heroName="Wonder Woman"/>
+      <Welcome name="Diana" heroName="Wonder Woman"/> */}
+      {/* <Message /> */}
+      {/* <Greet/>
+     <Welcome/> */}
+      {/* <Greet name="Bruce" heroName="Batman">
+       <p>This is children props</p>
+      </Greet>
+     <Greet name="Clark" heroName="Superman">
+       <button>Action</button>
+     </Greet>
+     <Greet name="Diana" heroName="Wonder Woman"/>
+     <Welcome name="Bruce" heroName="Batman"/>
+     <Welcome name="Clark" heroName="Superman"/>
+     <Welcome name="Diana" heroName="Wonder Woman"/>
+     <Hello/> */}
     </div>
   );
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+
+class Counter extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             count:0
+        }
+    }
+    increment(){
+        // Never change the state directly
+        // this.state.count=this.state.count + 1
+        // console.log(this.state.count)
+        // this.setState({
+        //     count: this.state.count + 1
+        // },()=>{
+        //     console.log('Callback value', this.state.count)
+        // })
+        // //This console log dont put the right value because the set state is asinchronic
+        // console.log(this.state.count)
+        this.setState((prevState)=>({
+            count:prevState.count+1
+
+        }))
+      
+    }
+
+    incrementFive(){
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+        this.increment()
+    }
+    
+    render() {
+        return (
+            <div>
+                 <div>
+              Count - {this.state.count}
+            </div>
+            <button onClick={()=>this.incrementFive()}>Increment</button>
+
+            </div>
+           
+        )
+    }
+}
+
+export default Counter
